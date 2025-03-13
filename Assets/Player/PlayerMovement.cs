f51dc53d18cddd6b3712ccaf4211d89cc17cbc5b
Leon CARTER <leoncarter4200@gmail.com>
2025-03-13 11:59:17 +0100

    Player Controls
    - Sprint
    - Crouch
    - Jump

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -2,16 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
-
-using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float crouchSpeed = 3f;
+    public float sprint = 7f;
+    public float defaultSpeed = 5f;
+    public float jumpStrength = 10f;
+    public float gravity = -10f;
     public float mouseSensitivity = 2f;
 
+    public bool isCrouching = false;
+    public bool isRunning = false;
+
+    //isGrounded
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    private Vector3 velocity;
+
+    public Camera cam;
+    public float cameraMovement;
+
     private CharacterController controller;
+    public Rigidbody rb;
     private float xRotation = 0f;
     public Transform playerCamera; // Référence manuelle à la caméra
 
@@ -25,9 +41,66 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         Move();
         LookAround();
+        Sprint();
+        Crouch();
+        Jump();
+        Gravity();
     }
+
+    void Sprint()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && isCrouching == false)
+        {
+            isRunning = true;
+            speed = sprint;
+        }
+        else if (Input.GetKey(KeyCode.LeftShift) == false && isCrouching == false)
+        {
+            isRunning = false;
+            speed = defaultSpeed;
+            cam.transform.localPosition = new Vector3(cam.transform.localPosition.x, 0f, cam.transform.localPosition.z);
+        }
+    }
+
+    void Crouch()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && isRunning == false)
+        {
+            isCrouching = true;
+            speed = crouchSpeed;
+            cam.transform.localPosition = new Vector3(cam.transform.localPosition.x, -0.7f, cam.transform.localPosition.z);
+        }
+        else if (Input.GetKey(KeyCode.LeftControl) == false && isRunning == false)
+        {
+            isCrouching = false;
+            speed = defaultSpeed;
+            cam.transform.localPosition = new Vector3(cam.transform.localPosition.x, 0f, cam.transform.localPosition.z);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpStrength * -1.5f * gravity);
+        }
+    }
+
+    void Gravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
 
     void Move()
     {
@@ -36,6 +109,11 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+
+        if(isRunning == true)
+        {
+            cam.transform.position = cam.transform.position ;
+        }
     }
 
     void LookAround()
