acea57e222920396476d70cc0b9deaec960a9b63
elantoinebasset <antoine.basset17@orange.fr>
2025-03-14 14:49:04 +0100

    Update_Message_Pickup

--- Assets/Player/PickupTimeSwap.cs
+++ Assets/Player/PickupTimeSwap.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    public GameObject PickupText;
     public GameObject TimeSwapOnPlayer;
     // Start is called before the first frame update
     void Start()
     {
         TimeSwapOnPlayer.SetActive(false);
+        PickupText.SetActive(false);
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            PickupText.SetActive(true);
+
             if (Input.GetKeyDown(KeyCode.E))
             {
                 this.gameObject.SetActive(false);
                 
                 TimeSwapOnPlayer.SetActive(true);
+
+                PickupText.SetActive(false);
             }
         }
     }
