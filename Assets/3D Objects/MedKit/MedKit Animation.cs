b49c30ee892fccd1328778f906c27a7bca0b48fd
deadzifixMMI <paul.pauly@mmibordeaux.com>
2025-03-20 19:37:27 +0100

    commit

---
+++ Assets/3D Objects/MedKit/MedKit Animation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationMedKit : MonoBehaviour
+{
+    private Animator animator;
+    private bool hasClickedLeft = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !hasClickedLeft) 
+        {
+            animator.SetBool("isPlaying", true);
+            hasClickedLeft = true; 
+
+        }
+    }
+}
+
