e2cc47b9180fa6e5d8a4454229cfadf5b2933156
deadzifixMMI <paul.pauly@mmibordeaux.com>
2025-03-20 15:35:25 +0100

    Push

--- Assets/Player/Inventaire/InventorySystem.cs
+++ Assets/Player/Inventaire/InventorySystem.cs
@@ -4,12 +4,20 @@ using System.Collections.Generic;
 public class InventorySystem : MonoBehaviour
 {
     public static InventorySystem instance;
-
-    public List<GameObject> items = new List<GameObject>();
     public List<Sprite> itemIcons = new List<Sprite>();
     public List<GameObject> itemModels = new List<GameObject>(); // 🔹 Stockage des modèles 3D
 
     public int maxSlots = 9;
+
+    private void Start()
+    {
+        if (RoomManager.instance != null)
+        {
+            itemIcons = new List<Sprite>(RoomManager.instance.itemIcons);
+            itemModels = new List<GameObject>(RoomManager.instance.itemModels);
+            FindObjectOfType<InventoryUI>()?.RefreshUI();
+        }
+    }
 
     private void Awake()
     {
@@ -18,13 +26,12 @@ public class InventorySystem : MonoBehaviour
 
     public bool AddItem(GameObject item, Sprite icon, GameObject model)
     {
-        if (items.Count >= maxSlots)
+        if (itemModels.Count >= maxSlots)
         {
             Debug.Log("❌ Inventaire plein !");
             return false;
         }
 
-        items.Add(item);
         itemIcons.Add(icon);
         itemModels.Add(model); // 🔹 Ajoute le modèle 3D à la liste
         FindObjectOfType<InventoryUI>()?.RefreshUI();
